40e2bbb186a13672710e6c9b958c64fd6aa3ec74
aikfiend <aik.fiend@gmail.com>
2021-02-17 01:04:18 +0200

    Optimizes imports, adds variable to handle JUnit version in gradle dependencies

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -2,20 +2,20 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
-
     @Test
     void selenideGoogleTest() {
-        open("https://www.google.com/");
+        open("https://www.google.com");
         $(byName("q")).setValue("Selenide").pressEnter();
         $("#search").shouldHave(text("selenide.org"));
     }
 
     @Test
     void selenideYandexTest() {
-        open("https://yandex.ru/");
+        open("https://yandex.ru");
         $("#text").val("Selenide").pressEnter();
         $("div #search-result").shouldHave(text("Selenide: concise UI tests in Java"));
     }
